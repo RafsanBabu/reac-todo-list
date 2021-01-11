06da4085829ad9dd58720c5684a5af86c43c3c77
RafsanBabu <rafsan.cse.ru13@GMAIL.COM>
2021-01-11 12:32:17 +0600

    custom style added

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -7,7 +7,7 @@ const TodoList = (props) => {
             <div className="d-flex list">
             <p>{props.taskitem.value}</p>
             <button className="delete" onClick={(e) =>props.handleDelete(e, props.taskitem.id)}>Delete</button>
-            <button className="completed" onClick={(e) =>props.handleComplete(e, props.taskitem.id)}>Completed</button>
+            <button className={props.taskitem.isCompleted ? "crossText" : "completed"} onClick={(e) =>props.handleComplete(e, props.taskitem.id)}>Completed</button>
             </div>
             
         </div>
